31e8387042224d32dc23ff13d5587b268f179378
Galih Adhi Kusuma <noxturne.production@gmail.com>
2024-11-05 20:36:31 +0700

    auto redirect to /login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,13 @@
 <script>
+	import { onMount } from "svelte";
+
 	let count = 0;
 
 	function increment() {
 		count += 1;
 	}
+
+	onMount(() => {
+		window.location.href = "/login"
+	})
 </script>
-
-<button on:click={increment}>
-	{count === 1 ? 'time' : 'times'}
-</button>
-
-<h1>Welcome to nightmare</h1>
-<h2>but just kidding! yey</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<style>
-	button {
-		padding: 10px 20px;
-		font-size: 16px;
-		border-radius: 5px;
-		background-color: lightblue;
-		border: none;
-		cursor: pointer;
-	}
-</style>
