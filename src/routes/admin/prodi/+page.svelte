57a6033c35e1cfeb1e2849d0ba58252a91c9162a
mvplayea <cjllakv@gmail.com>
2025-01-02 00:21:35 +0700

    API get prodi

---
+++ src/routes/admin/prodi/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+
+	let prodiData;
+
+	async function getDataProdi() {
+		try {
+			const response = await axios.get('/api/prodi');
+			prodiData = await response.data.prodi[0];
+			console.log(prodiData);
+		} catch (error) {
+			console.error('Error:', error);
+		}
+	}
+
+	onMount(async () => {
+		console.log('halo');
+		await getDataProdi();
+	});
+</script>
+
+<section>
+	{#if prodiData}
+		<p>{prodiData[0].kd_prodi}</p>
+	{/if}
+</section>
--- src/routes/api/prodi/+server.js
+++ src/routes/api/prodi/+server.js
@@ -1,13 +1,14 @@
-import connection from "$lib/db.js";
-import {json} from "@sveltejs/kit";
+import connection from '$lib/db.js';
+import { json } from '@sveltejs/kit';
+import { getParams } from '$lib/util';
 
 export async function GET({ url }) {
-  const nim = url.searchParams.get('nim');
-  
-  const prodi = await connection.query(
-    `SELECT kd_prodi, nama_prodi
-    FROM prodi`
-  );
-  
-  return json({ prodi });
+	const kd_prodi = await getParams(url, 'kd_prodi');
+	if (kd_prodi) {
+		const prodi = await connection.query(`select * from prodi where kd_prodi=${kd_prodi}`);
+		return json({ prodi });
+	} else {
+		const prodi = await connection.query(`select * from prodi`);
+		return json({ prodi });
+	}
 }
